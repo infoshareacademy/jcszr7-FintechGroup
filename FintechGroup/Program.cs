050b796e19672ec43c3d58b14513b096c4f7c352
Joanna Musialkowska <jzmusialkowska@gmail.com>
2022-10-07 20:17:10 +0200

    Initial commit

--- FintechGroup/Program.cs
+++ FintechGroup/Program.cs
@@ -1,7 +1,16 @@
-﻿Console.WriteLine("Witaj, Użytkowniku, w aplikacji FinTech. Co chciałbyś dzisiaj zrobić?\n");
+﻿using FintechGroup;
 
-Console.WriteLine("1. Wyświetl istniejące rekordy\n" +
-                  "2. Zarządzaj rekordami\n" +
-                  "3. Wyszukaj lub filtruj \n" +
-                  "4. Zakończ działanie aplikacji\n");
+int[] mainMenuValues = new int[] { 1, 2, 3, 4 };
+Menu.DisplayMenuGreeting();
+Menu.DisplayMainMenu();
+int mainMenuOption = int.Parse(Console.ReadLine());
+Menu.SelectMainMenuOption(mainMenuOption);
+while (!mainMenuValues.Contains(mainMenuOption))
+{    
+    Menu.DisplayMainMenu();
+    mainMenuOption = int.Parse(Console.ReadLine());
+    Menu.SelectMainMenuOption(mainMenuOption);
+}
+
+
 
