a126781dbf636697571acc904e965fe4f3fd5d52
N-SAID <i.doumen@n-said.pl>
2022-10-16 13:04:14 +0200

    fix plik menu

--- FintechGroup.Services/Menu.cs
+++ FintechGroup.Services/Menu.cs
@@ -1,12 +1,14 @@
 ﻿using FintechGroup.Database;
+using FintechGroup.Services;
+
 namespace FintechGroup
 {
-
     public class Menu
     {
 
         public static void DisplayMenuGreeting()
         {
+            Console.WriteLine("Witaj, Użytkowniku, w aplikacji FinTech. Co chciałbyś dzisiaj zrobić?\n");
 
         }
 
@@ -15,8 +17,21 @@ namespace FintechGroup
             Console.WriteLine("Czy na pewno chcesz zakończyć działanie aplikacji?\n" +
                 "Naciśnij klawisz 't', jeśli chcesz zakończyć działanie programu.\n" +
                 "Naciśnij klawisz 'n', aby powrócić do menu głównego.");
-        }
+            string v = Console.ReadLine();
 
+            if (v == "t")
+            {
+
+                Console.Clear();
+                Console.WriteLine("Aplikacja zostane zakonczona");
+                Exit.ExitMenu();
+            }
+            else
+            {
+                Console.Clear();
+                DisplayMainMenu();
+            }
+        }
         public static void DisplayMainMenu()
         {
             Console.WriteLine("1. Wyświetl istniejące rekordy\n" +
@@ -24,13 +39,12 @@ namespace FintechGroup
             "3. Wyszukaj lub filtruj \n" +
             "4. Zakończ działanie aplikacji\n\n" +
             "Napisz numer preferowanej opcji:");
-
+            Menu2.CallMenu();
         }
 
         public static void SelectMainMenuOption(int key)
         {
             switch (key)
-
             {
                 case 1:
                     Console.Clear();
@@ -48,7 +62,26 @@ namespace FintechGroup
                     Console.WriteLine("1. Wyszukaj");
                     Console.WriteLine("2. Filtruj");
                     Console.WriteLine("3. Wróć do menu głównego");
+                    int vkey = int.Parse(Console.ReadLine());
 
+                    switch (vkey)
+                    {
+
+                        case 1:
+                            Console.Clear();
+                            Wzszukiwanie.WzszukiwanieMenu();
+                            break;
+
+                        case 2:
+                            Console.Clear();
+                            Filtracja.FiltracjaMenu();
+                            break;
+
+                        case 3:
+                            Console.Clear();
+                            DisplayMainMenu();
+                            break;
+                    }
                     break;
                 case 4:
                     Console.Clear();
@@ -56,9 +89,7 @@ namespace FintechGroup
                     break;
                 default:
                     Console.Clear();
-
                     Console.WriteLine("Wybierz prawidłową opcję z przedziału 1-4.\n");
-
                     break;
             }
         }
