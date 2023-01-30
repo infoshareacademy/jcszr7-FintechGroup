927f59beb27f8cbdae81b8f8ea3502ff32d1ef14
Przemek Lewandowski <przemyslaw.lewandowski.91@gmail.com>
2023-01-30 19:20:39 +0100

    Added bootstrap, basic menu, working search bar, basic sorting, connection to database and CRUD.

---
+++ FinTechGroup.Persistance.EntityFramework/Data/Migrations/20230122090957_NewProject.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinTechGroup.Web.Data.Migrations
+{
+    public partial class NewProject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
